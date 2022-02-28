3f915b906ed021ff82025a9cc07a20bc8c538c47
Nerys-Thamm <nerysthamm@mgail.com>
2022-03-01 00:41:01 +1300

    Made login sessions

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./route');
+const app = express();
+const sessions = require('express-session');
+const cookieParser = require('cookie-parser');
+
+app.use(sessions({
+    secret: 'secret',
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+    resave: false
+    }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', route);
+app.use((req, res, next) => { res.redirect('/dashboard') });
+const server = http.createServer(app);
+server.listen(3000, ()=>{console.log('Server is running on port 3000');});
+
---
+++ route.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+
+var session;
+
+let db = new sqlite3.Database(path.join(__dirname, '/db/database.db'), (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+
+
+router.get('/signup', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'signup.html'));
+    
+});
+router.post('/signup', (req, res, next) => {
+    let db = new sqlite3.Database(path.join(__dirname, '/db/database.db'));
+    db.all('INSERT INTO users (username, password) VALUES (?, ?)', [req.body.username, req.body.password], (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('A row has been inserted.');
+    })
+    db.close();
+    res.redirect('/signin');
+});
+    
+
+router.get('/signin', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'signin.html'));
+    
+    });
+
+router.post('/signin', (req, res, next) => {
+    let db = new sqlite3.Database(path.join(__dirname, '/db/database.db'));
+    db.get('SELECT password FROM users WHERE username = ?', [req.body.username], (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        if (row) {
+            if(row.password == req.body.password){
+                session = req.session;
+                session.userid = req.body.username;
+                console.log(req.session);
+                res.redirect('/dashboard');
+            }
+            else{
+                res.send('Wrong password');
+            }
+        }
+        else {
+            res.sendFile(path.join(__dirname, 'signup.html'));
+        }
+    });
+    db.close();
+});
+
+router.get('/logout', (req, res, next) => {
+    req.session.destroy();
+    res.redirect('/signin');
+});
+
+router.get('/dashboard', (req, res, next) => {
+    session = req.session;
+    if(session.userid)
+    {
+        res.sendFile(path.join(__dirname, 'dashboard.html'));
+    }
+    else{
+        res.redirect('/signin');
+    }
+});
+
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+    module.exports = router;
+    
+
