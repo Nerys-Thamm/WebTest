70852f07599ea85721b37366b1971c56b0581d26
Nerys-Thamm <nerysthamm@mgail.com>
2022-05-31 18:37:44 +1200

    ddddd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(sessions({
     secret: 'secret',
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: { maxAge: 120000 },
     resave: false
     }));
