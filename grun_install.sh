8393056d470de1cc6b3c7be1fd448c491d8c9023
Nerys-Thamm <nerysthamm@gmail.com>
2022-05-31 03:42:23 +0000

    hm

--- grun_install.sh
+++ grun_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo """#!/bin/bash
+echo '''#!/bin/bash
 git pull
 clear
 echo 'Project ready!'
-node $1
-""" >> grun.sh
+node "$1"
+''' >> grun.sh
 chmod +x grun.sh
 echo "alias grun='./grun.sh'" >> ~/.bashrc
