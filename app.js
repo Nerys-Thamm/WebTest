55fac8c27882e2263e40c2014c72e9d58c3e1796
Nerys-Thamm <nerysthamm@mgail.com>
2022-05-31 17:48:47 +1200

    Fixed export

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./route');
-
+const db = require ('./db/database');
 const app = express();
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -15,11 +15,11 @@ app.use(sessions({
     cookie: { maxAge: 120000 },
     resave: false
     }));
-app.use(route.db.passport.initialize());
-app.use(route.db.passport.session());
-route.db.passport.use(new route.db.LocalStrategy(route.db.User.authenticate()));
-route.db.passport.serializeUser(route.db.User.serializeUser());
-route.db.passport.deserializeUser(route.db.User.deserializeUser());
+app.use(db.passport.initialize());
+app.use(db.passport.session());
+db.passport.use(new db.LocalStrategy(db.User.authenticate()));
+db.passport.serializeUser(db.User.serializeUser());
+db.passport.deserializeUser(db.User.deserializeUser());
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
